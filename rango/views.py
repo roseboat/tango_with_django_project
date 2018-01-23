9906a66567f4acec91ba67b28b3d3932e7d80135
Rosa Downing <0908715d@student.gla.ac.uk>
2018-01-23 15:21:28 +0000

    Commiting changes

--- rango/views.py
+++ rango/views.py
@@ -8,3 +8,11 @@ def index(request):
 
     return render(request, 'rango/index.html', context=context_dict)
     return HttpResponse("Rango says hey there partner!")
+    
+
+def about(request):
+
+    context_dict = {'boldmessage': "ABOUT PAGE"}
+    
+    return render(request, 'rango/about.html', context=context_dict)
+    return HttpResponse("Rango says here is the about page")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -13,19 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
-django.conf import settings
-django.conf.urls.static import static
+
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
-    # above maps any URLS starting
-    # with rango/ to be handled by
-    # the rango application
     url(r'^admin/', admin.site.urls),
-] + (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
